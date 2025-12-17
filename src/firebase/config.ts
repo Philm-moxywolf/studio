1802de230ed8e28da7996af0ca79cbc9f537c6fb
Philip Mudhir <phil@elevateai.tech>
2025-12-17 11:04:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4325813636-6b51f",
+  "appId": "1:320922878926:web:c96d011416a63f42fea19f",
+  "apiKey": "AIzaSyA9tvj6sXbfOXgsTB8Q9v9M9wsnijUMrfE",
+  "authDomain": "studio-4325813636-6b51f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "320922878926"
+};
